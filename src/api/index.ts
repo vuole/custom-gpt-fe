33e3ad746460cbe2b1bd83667d8f16750b9e46cd
Vu Le Van <vuole.it@gmail.com>
2023-12-06 00:02:28 +0700

    continue...

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = "https://trunggiangiaodich.vn:8443/api";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://custom-gpt-be.vercel.app";
 
 export const HEADERS = {
   headers: { "content-type": "application/json; charset=UTF-8" },
